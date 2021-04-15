bb9aa03d6bdbbb9c4c11eb2b0c14c5743d57e6e2
lyh543 <lyh543@outlook.com>
2021-04-15 08:52:59 +0800

    完成校内打卡

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # UESTC 健康打卡平台自动打卡脚本
 
 [![Auto-report action](https://github.com/lyh543/UESTC-ncov-AutoReport/actions/workflows/report.yml/badge.svg)](https://github.com/lyh543/UESTC-ncov-AutoReport/actions/workflows/report.yml)
-![Language](https://img.shields.io/badge/Python-3.9-yellow.svg)
+![Language](https://img.shields.io/badge/Python-3.9-blue.svg)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 <!-- ![GitHub stars](https://img.shields.io/github/stars/lyh543/UESTC-ncov-AutoReport)
 ![GitHub forks](https://img.shields.io/github/forks/lyh543/UESTC-ncov-AutoReport) -->
@@ -16,7 +16,7 @@
 
 暂无更新。
 
-## 使用方法
+## 使用方法 (GitHub Actions)
 
 0. **写在前面：请在自己 Fork 的仓库中修改，并 Rush 到自己的仓库，不要直接修改本仓库，也不要将您的修改 Pull Request 到本仓库（对本仓库的改进除外）！如果尚不了解 GitHub 的基本使用方法，请参阅 [使用议题和拉取请求进行协作/使用复刻](https://docs.github.com/cn/github/collaborating-with-issues-and-pull-requests/working-with-forks) 和 [使用议题和拉取请求进行协作/通过拉取请求提议工作更改](https://docs.github.com/cn/github/collaborating-with-issues-and-pull-requests/proposing-changes-to-your-work-with-pull-requests)。**
 
@@ -26,16 +26,20 @@
 4. 点击 Settings 选项卡，点击左侧 Secrets，点击 New Secret，创建一个 Secret 名为 `CONFIG`，值如下（替换为自己的信息）：
 
 ```
-{"sn": "学号", "idCard": "身份证后六位", "avatarurl": "（选填）", "nickname": "（选填）", "location": "qingshuihe or shahe"}
+{"sn": "学号", "idCard": "身份证后六位", "avatarurl": "头像链接（选填）", "nickname": "微信昵称（选填）", "location": "地址（选填）"}
 ```
+
+其中 `location` 的值可以自定义（亦可不填），以下提供几个参考：
+* 清水河：`四川省郫都区S9`
 
 ![secrets](images/1fc63d7f6843687acd6261283d49fc740d7a06235e7f513344626c96a5654a2c.png)  
 
-4. 默认的打卡时间是每天的早上 9:30，如需选择其它时间，可以修改 `.github/workflows/report.yml` 中的 `cron`，详细说明参见[安排的事件](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)，请注意这里使用的是**国际标准时间 UTC**，北京时间的数值比它大 8 个小时。建议修改默认时间，避开打卡高峰期以提高成功率。
-5. 在 Actions 选项卡可以确认打卡情况。如果打卡失败（可能是临时网络问题等原因），脚本会自动重试，五次尝试后如果依然失败，将返回非零值提示构建失败。
-6. 在 GitHub 个人设置页面的 Notifications 下可以设置 Github Actions 的通知，建议打开 Email 通知，并勾选 `Send notifications for failed workflows only`。
 
-## 在本地运行测试
+5. 默认的打卡时间是每天的早上 9:30，如需选择其它时间，可以修改 `.github/workflows/report.yml` 中的 `cron`，详细说明参见[安排的事件](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)，请注意这里使用的是**国际标准时间 UTC**，北京时间的数值比它大 8 个小时。建议修改默认时间，避开打卡高峰期以提高成功率。
+6. 在 Actions 选项卡可以确认打卡情况。如果打卡失败（可能是临时网络问题等原因），脚本会自动重试，五次尝试后如果依然失败，将返回非零值提示打卡失败。
+7. 在 GitHub 个人设置页面的 Notifications 下可以设置 Github Actions 的通知，建议打开 Email 通知，并勾选 `Send notifications for failed workflows only`。
+
+## 本地测试方法
 
 要在本地运行测试，需要安装 Python 3。我们假设您已经安装了 Python 3 和 pip3，并已将其路径添加到环境变量。
 
@@ -57,6 +61,15 @@ pip install -r requirements.txt
 python report.py
 ```
 
-## 许可
+## 无法登陆小程序
 
-MIT License
+这可能是由于 GitHub Actions 成功登陆，但退出失败，导致小程序无法登陆。请查询 GitHub Actions 中成功登陆的 Session id，然后手动退出。可以在项目根目录运行 python 执行以下代码退出登陆：
+
+```py
+from report import Report
+auto_report = Report()
+auto_report.session.cookies['SESSION'] = 'Your Session id'
+auto_report.logout()
+```
+
+如果仍无法登录小程序，请联系辅导员/管理员。
--- report.py
+++ report.py
@@ -1,4 +1,6 @@
 # encoding=utf8
+from time import sleep
+
 import requests
 import os
 import json
@@ -20,35 +22,59 @@ class Report(object):
             "Referer": "https://servicewechat.com/wx521c0c16b77041a0/28/page-frame.html"  # 小程序的 id
         }
         # 读取配置文件
-        with open('config.json', 'r') as config_file:
+        with open('config.json', 'r', encoding='utf-8') as config_file:
             self.data = json.load(config_file)
         # 确认坐标
-        self.location = self.data.pop('location', 'qingshuihe')
-        if self.location == 'qingshuihe':
-            pass
-        elif self.location == 'shahe':
-            raise NotImplementedError
-        else:
-            raise ValueError(f'location 参数错误: {self.location}')
+        self.location = self.data.pop('location', '')
 
     # 登陆/绑定
     def login(self) -> bool:
-        response_checkbind = self.session.post(self.baseUrl + "/wxvacation/api/epidemic/login/checkBind", json={},
-                                               headers=self.headers)  # 获取 SESSION，否则登录不上
-        response_login = self.session.post(self.baseUrl + "/wxvacation/api/epidemic/login/bindUserInfo", json=self.data,
+        self.session.post(self.baseUrl + "/wxvacation/api/epidemic/login/checkBind", json={}, 
+                          headers=self.headers)  # 获取 SESSION，否则登录不上
+        response = self.session.post(self.baseUrl + "/wxvacation/api/epidemic/login/bindUserInfo", json=self.data,
                                            headers=self.headers)  # 登录，并将 SESSION ID 和用户绑定
         # 输出 SESSION ID。要是忘记解绑，小程序无法登陆，就得拿 SESSION ID 解绑
-        if response_login.json()['code'] == 0:
+        if response.json()['code'] == 0:
             session_id = self.session.cookies["SESSION"]
             print(f'登录成功，session 为 {session_id}，请妥善保管！')
             return True
         else:
-            print(f'登录失败，response 为 {response_login.json()}')
+            print(f'登录失败，response 为 {response.json()}')
             return False
 
     # 打卡
     def report(self) -> bool:
-        return True
+        # 检查是否已经打卡
+        # 重复打卡会报数据库错，外包的代码 异常处理还是 8 行
+        response = self.session.get(self.baseUrl + "/wxvacation/checkRegisterNew", headers=self.headers)
+        if not response.ok:
+            print(f'获取打卡状态失败：{response.status_code}, text 为：{response.text}')
+            return False
+        if response.json()['data']['appliedTimes'] == 1:
+            print('今日已打卡，跳过打卡')
+            return True
+        print('今日未打卡，开始打卡...')
+        if response.json()['data']['schoolStatus'] == 1:    # 在校
+            return self.report_in_school()
+        else:
+            raise NotImplementedError('暂未实现校外打卡')
+
+    # 校内打卡
+    def report_in_school(self) -> bool:
+        data = {
+            "healthCondition":"正常",
+            "todayMorningTemperature":"36°C~36.5°C",
+            "yesterdayEveningTemperature":"36°C~36.5°C",
+            "yesterdayMiddayTemperature":"36°C~36.5°C",
+            "location":self.location
+        }
+        response = self.session.post(self.baseUrl + "/wxvacation/monitorRegisterForReturned", json=data, headers=self.headers)
+        if response.json()['code'] == 0:
+            print(f'校内打卡成功，“请明日 10:00 之前继续填写~”')
+            return True
+        else:
+            print(f'校外打卡失败，response 为 {response.json()}')
+            return False
 
     # 注销/解绑
     # 每次打完卡都必须解绑 session_id 和账户，否则下次登录不上
@@ -69,24 +95,21 @@ class Report(object):
                 ret = False
             if not self.logout():
                 ret = False
-        except:
-            ret = False
+        except Exception as e:
+            self.logout()
+            raise e
         return ret
 
 
 if __name__ == "__main__":
     auto_reporter = Report()
-    count = 5
-    while count != 0:
+    for i in range(1, 6):
         ret = auto_reporter.run()
         if ret:
-            break
-        print("打卡失败，重试中...\n")
-        count = count - 1
-
-    if count != 0:
-        print('打卡成功')
-        exit(0)
-    else:
-        print('打卡失败，退出程序')
-        exit(-1)
+            print('程序执行成功，退出')
+            exit(0)
+        if i == 5:
+            print('程序连续 5 次执行失败，退出')
+            exit(-1)
+        sleep(1)
+        print(f"程序第 {i} 次执行失败，重试中...\n")
